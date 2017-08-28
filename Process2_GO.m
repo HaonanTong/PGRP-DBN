a36396ae309dab81c463d91c3f4875ce5acc41bc
htong <htong@scorpius.ece.ncsu.edu>
2017-08-28 19:12:46 -0400

    pie

--- Process2_GO.m
+++ Process2_GO.m
@@ -8,5 +8,14 @@ for i = 1 : length(myFile)
     file = sprintf('%s/%s',myFile(i).folder,myFile(i).name);
     file_go = strcat(myFile(i).folder,'/hubgenes_go');
     unix(sprintf('awk -F "." ''{print $1}'' %s | sort -u > %s.txt',file, file_go));
-    unix(sprintf('awk ''END{print NR}'' %s.txt > %s.txt',file_go,'hubgenes_num'));
+    file_go_num = strcat(myFile(i).folder,'/hubgenes_num');
+    unix(sprintf('awk ''END{print NR}'' %s.txt > %s.txt',file_go,file_go_num));
+end
+
+%%
+myFile = dir(fullfile('./','A_Stage*/hubgenes_go.txt'));
+for i = 1 : length(myFile) 
+    glist = sprintf('%s/%s',myFile(i).folder,myFile(i).name);
+    glistgo = sprintf('%s/%s',myFile(i).folder,'hubgenes_go_discripition.txt');
+    unix(sprintf('grep -f %s ATH_GO_GOSLIM.txt | sort -u > %s',glist,glistgo));
 end
